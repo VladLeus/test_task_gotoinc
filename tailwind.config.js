812247f39a3719a0fb1b22b37f4405bf2e612ce9
VladLeus <vladyslavleus@gmail.com>
2023-11-06 14:54:39 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'main-color': '#254E70',
+        'second-color': '#37718E',
+        'third-color': '#8EE3EF',
+        'fourth-color': '#AEF3E7',
+        'fifth-color': '#C33C54'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
